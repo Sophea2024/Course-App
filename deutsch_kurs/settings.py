4c36ecca7c24c89e3e5866e3a6c781d74e31441e
unknown <Sophea.Wiesenfarth-Chan@gmx.de>
2025-06-06 17:14:00 +0200

    Update html files

--- deutsch_kurs/settings.py
+++ deutsch_kurs/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^sm!d4e@9!4fsxm!#93f0@*_*_lahgq(q)-ji1!8*s3+49x0$9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['course-app-p4rq.onrender.com']
 
 
 # Application definition
